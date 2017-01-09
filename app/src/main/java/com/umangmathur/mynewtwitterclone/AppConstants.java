19189103b11a60fa5473ce29055a11c46ed82b6f
Umang Mathur <umangmathur10051992@gmail.com>
2017-01-09 18:05:26 +0530

    Update AppConstants.java

--- app/src/main/java/com/umangmathur/mynewtwitterclone/AppConstants.java
+++ app/src/main/java/com/umangmathur/mynewtwitterclone/AppConstants.java
@@ -4,8 +4,8 @@ package com.umangmathur.mynewtwitterclone;
  * Created by umang on 27/9/15.
  */
 public interface AppConstants {
-    String TWITTER_KEY = "x1PksJvvMzvD3P1AWEVS2kFOY";
-    String TWITTER_SECRET = "g2O30FAWIqB6yDWJo7dCzRYX4vCD3IuKG9uNbGLTpg4tEXMeSJ";
+    String TWITTER_KEY = "G73xcNa35IDyt7I6dbI1YYpiP";
+    String TWITTER_SECRET = "u7shUlidaClZMazAEGXYE05U6oDGkYQv90FRtiu11JbAUqIqwa";
     String MY_NEW_TWITTER_CLONE = "My New Twitter Clone";
     int MAX_CHARACTERS = 140;
     int NUM_OF_TWEETS = 50;
